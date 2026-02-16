d8a950e566636a24f6a3ac75d1efe1f943b64205
genspark_dev <genspark_dev@genspark.ai>
2026-02-16 14:21:33 -0800

    fix: import useMemo in DebateDashboard.tsx

--- src/app/[lang]/debate/DebateDashboard.tsx
+++ src/app/[lang]/debate/DebateDashboard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, use } from 'react';
+import React, { useState, useEffect, useMemo, use } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
